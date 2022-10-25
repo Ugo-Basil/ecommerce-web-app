4db2f7f1343a7b95f5b82100ffee62d070ce27d1
Basil <ohuruogubasil@gmail.com>
2022-10-26 00:34:01 +0100

    navbar/homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
+import Navbar from '../../components/navbar/Navbar'
+import banner from '../../assets/banner.jpg'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Container>
+      <Navbar />
+      <Banner>
+        <img src={banner} alt="" />
+      </Banner>
+    </Container>
   )
 }
 
+
+const Container = styled.div`
+  width: 100%;
+`
+
+const Banner = styled.div`
+  width: 100%;
+  height: 300px;
+  img{
+    width: 100%;
+    -webkit-mask-image: linear-gradient(to bottom, rgba(0,0,0,2), rgba(0,0,0,0.95), rgba(0,0,0,0.85), rgba(0,0,0,0.75),rgba(0,0,0,0.55), rgba(0,0,0,0));
+  }
+`
 export default Home
